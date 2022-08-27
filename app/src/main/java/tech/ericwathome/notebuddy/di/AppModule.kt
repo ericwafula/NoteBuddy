4410b771c7011783e274f24cfe6654de022b6b00
ericwafula <ericwathome007@gmail.com>
2022-08-27 13:35:37 +0300

    added initial appModule dependencies

--- app/src/main/java/tech/ericwathome/notebuddy/di/AppModule.kt
+++ app/src/main/java/tech/ericwathome/notebuddy/di/AppModule.kt
@@ -1,12 +1,43 @@
 package tech.ericwathome.notebuddy.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import tech.ericwathome.notebuddy.data.remote.NoteBudyApiService
+import tech.ericwathome.notebuddy.util.BASE_URL
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .client(httpClient)
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideNoteBudyApiService(retrofit: Retrofit): NoteBudyApiService = retrofit.create(NoteBudyApiService::class.java)
 
 
 
