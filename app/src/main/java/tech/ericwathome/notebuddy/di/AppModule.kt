a50acec257819c9aef9ec2d27fa16812dbb65023
ericwafula <ericwathome007@gmail.com>
2022-08-27 12:23:39 +0300

    added initial appModule config

---
+++ app/src/main/java/tech/ericwathome/notebuddy/di/AppModule.kt
@@ -0,0 +1,10 @@
+package tech.ericwathome.notebuddy.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+}
