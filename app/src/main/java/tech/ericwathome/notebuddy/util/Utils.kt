431533ebe8e11837f2d4527086678996ceff81b0
ericwafula <ericwathome007@gmail.com>
2022-08-27 12:50:07 +0300

    added Toast extension for context classes

--- app/src/main/java/tech/ericwathome/notebuddy/util/Utils.kt
+++ app/src/main/java/tech/ericwathome/notebuddy/util/Utils.kt
@@ -1,2 +1,8 @@
 package tech.ericwathome.notebuddy.util
 
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
