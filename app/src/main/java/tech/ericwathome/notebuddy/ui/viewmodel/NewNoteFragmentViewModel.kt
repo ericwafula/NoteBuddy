fe49734a79a3ec91fa12e3937e20b9002790355b
ericwafula <ericwathome007@gmail.com>
2022-08-27 14:55:54 +0300

    added addNote functionality in NewNoteFragmentViewModel.kt

--- app/src/main/java/tech/ericwathome/notebuddy/ui/viewmodel/NewNoteFragmentViewModel.kt
+++ app/src/main/java/tech/ericwathome/notebuddy/ui/viewmodel/NewNoteFragmentViewModel.kt
@@ -1,6 +1,25 @@
 package tech.ericwathome.notebuddy.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import tech.ericwathome.notebuddy.data.dto.NoteDto
+import tech.ericwathome.notebuddy.data.remote.repository.NoteRepository
+import javax.inject.Inject
 
-class NewNoteFragmentViewModel : ViewModel() {
+@HiltViewModel
+class NewNoteFragmentViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
+    private val _message = MutableStateFlow<String?>(null)
+    val message = _message.asStateFlow()
+
+    fun addNote(noteDto: NoteDto) {
+        viewModelScope.launch {
+            val response = repository.addNote(noteDto)
+            _message.value = response.message
+        }
+    }
+
 }
--- app/src/main/java/tech/ericwathome/notebuddy/ui/viewmodel/NotesFragmentViewModel.kt
+++ app/src/main/java/tech/ericwathome/notebuddy/ui/viewmodel/NotesFragmentViewModel.kt
@@ -1,6 +1,10 @@
 package tech.ericwathome.notebuddy.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import tech.ericwathome.notebuddy.data.remote.repository.NoteRepository
+import javax.inject.Inject
 
-class NotesFragmentViewModel : ViewModel() {
+@HiltViewModel
+class NotesFragmentViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
 }
